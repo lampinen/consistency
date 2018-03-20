ce2ab20014f8072cd86e21f99199802345e4982b
Andrew Lampinen <lampinen@stanford.edu>
2018-03-19 19:21:59 -0700

    separating training variables, consistency

--- captioning/captioning.py
+++ captioning/captioning.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+
 from __future__ import division 
 
 import sys
@@ -27,6 +27,7 @@ config = {
     "sequence_length": 30,
     "word_embedding_dim": 128,
     "shared_word_embeddings": True, # whether input and output embeddings are the same or different
+                                    # weird things will happen in the consistency training if this isn't true
     "hidden_size": 256,
     "rnn_num_layers": 3,
     "full_train_every": 1, # a full training example is given once every _ training examples
@@ -85,11 +86,13 @@ def get_examples(n=None):
     for img in images:
         img_ann_ids = coco.getAnnIds(imgIds=img["id"])
         img_anns = coco.loadAnns(img_ann_ids)
+        captions =  [pad_or_trim(caption_to_words(x["caption"]), config["sequence_length"]) for x in img_anns]
+        captions = [{"caption": words_to_indices(x["words"], vocab), "mask": x["mask"]} for x in captions]
         this_datum = {
             "image_name": "{}/images/{}".format(config["coco_data_dir"],
                                                 img["file_name"]),
             "id": img["id"],
-            "captions": [words_to_indices(pad_or_trim(caption_to_words(x["caption"]), config["sequence_length"]), vocab) for x in img_anns] 
+            "captions": captions 
         }
         data.append(this_datum)
     return data
@@ -107,6 +110,7 @@ class captioning_model(object):
         self.mask_ph =  tf.placeholder(tf.bool, [config["batch_size"], config["sequence_length"]]) 
         self.caption2_ph = tf.placeholder(tf.int32, [config["batch_size"], config["sequence_length"]]) 
         self.mask2_ph =  tf.placeholder(tf.bool, [config["batch_size"], config["sequence_length"]]) 
+        self.neg_caption_ph = tf.placeholder(tf.int32, [config["batch_size"], config["sequence_length"]]) 
         self.lr_ph = tf.placeholder(tf.float32)
         self.keep_ph = tf.placeholder(tf.float32)
 
@@ -125,80 +129,151 @@ class captioning_model(object):
         self.image_rep = _build_perception_network(self.image_ph)
 
         # captioning
-        embedding_size = config['word_embedding_dim']
-        input_embeddings = tf.Variable(tf.random_uniform([vocab_size, embedding_size],
-                                                         -0.1/embedding_size, 0.1/embedding_size))
-        if config["shared_word_embeddings"]:
-            output_embeddings = input_embeddings
-        else:
-            output_embeddings = tf.Variable(tf.random_uniform([vocab_size, embedding_size],
-                                                              -0.1/embedding_size, 0.1/embedding_size))
+        with tf.variable_scope("basic_net"):
+            embedding_size = config['word_embedding_dim']
+            input_embeddings = tf.Variable(tf.random_uniform([vocab_size, embedding_size],
+                                                             -0.1/embedding_size, 0.1/embedding_size))
+            if config["shared_word_embeddings"]:
+                output_embeddings = input_embeddings
+            else:
+                output_embeddings = tf.Variable(tf.random_uniform([vocab_size, embedding_size],
+                                                                  -0.1/embedding_size, 0.1/embedding_size))
 
-        def _build_captioning_net(image_rep, reuse=True, keep_ph=None):
-            """Solves problem from problem embedding"""
-            with tf.variable_scope('captioning', reuse=reuse):
-                if keep_ph is not None:
-                    cell = lambda: tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.BasicLSTMCell(config['hidden_size']), output_keep_prob=keep_ph)
-                else:
-                    cell = lambda: tf.contrib.rnn.BasicLSTMCell(config['hidden_size'])
+            def _build_captioning_net(image_rep, reuse=True, keep_ph=None):
+                """Creates caption from visual embedding"""
+                with tf.variable_scope('captioning', reuse=reuse):
+                    if keep_ph is not None:
+                        cell = lambda: tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.BasicLSTMCell(config['hidden_size']), output_keep_prob=keep_ph)
+                    else:
+                        cell = lambda: tf.contrib.rnn.BasicLSTMCell(config['hidden_size'])
 
-                stacked_cell = tf.contrib.rnn.MultiRNNCell([cell() for _ in range(config['rnn_num_layers'])])
-                start_token = tf.nn.embedding_lookup(output_embeddings, vocab["<START>"])
-                word_logits = []
+                    stacked_cell = tf.contrib.rnn.MultiRNNCell([cell() for _ in range(config['rnn_num_layers'])])
+                    start_token = tf.nn.embedding_lookup(output_embeddings, vocab["<START>"])
+                    word_logits = []
+                    emb_outputs = []
 
-                state = stacked_cell.zero_state(config['batch_size'], tf.float32)
-                state = tuple([tf.contrib.rnn.LSTMStateTuple(image_rep, state[0][1])] + [state[i] for i in range(1, len(state))])
+                    state = stacked_cell.zero_state(config['batch_size'], tf.float32)
+                    state = tuple([tf.contrib.rnn.LSTMStateTuple(image_rep, state[0][1])] + [state[i] for i in range(1, len(state))])
 
-                emb_output = tf.reshape(start_token, [config['batch_size'], -1])
+                    emb_output = tf.reshape(start_token, [config['batch_size'], -1])
 
-                with tf.variable_scope("recurrence", reuse=reuse):
-                    output_to_emb_output = tf.get_variable(
-                        "output_to_emb_output",
-                        [config['hidden_size'], config['word_embedding_dim']],
-                        tf.float32)
-                    for step in range(config['sequence_length']):
-                        (output, state) = stacked_cell(emb_output, state)
-                        emb_output = tf.matmul(output, output_to_emb_output)
-                        this_word_logits = tf.matmul(emb_output, tf.transpose(output_embeddings))
-                        word_logits.append(this_word_logits)
-                        tf.get_variable_scope().reuse_variables()
+                    with tf.variable_scope("recurrence", reuse=reuse):
+                        output_to_emb_output = tf.get_variable(
+                            "output_to_emb_output",
+                            [config['hidden_size'], config['word_embedding_dim']],
+                            tf.float32)
+                        for step in range(config['sequence_length']):
+                            (output, state) = stacked_cell(emb_output, state)
+                            emb_output = tf.matmul(output, output_to_emb_output)
+                            emb_outputs.append(emb_output)
+                            this_word_logits = tf.matmul(emb_output, tf.transpose(output_embeddings))
+                            word_logits.append(this_word_logits)
+                            tf.get_variable_scope().reuse_variables()
 
-                word_logits = tf.stack(word_logits, axis=1)
-                return word_logits
+                    word_logits = tf.stack(word_logits, axis=1)
+                    emb_outputs = tf.stack(emb_outputs, axis=1)
+                    return word_logits, emb_outputs
 
-        self.caption_logits = _build_captioning_net(self.image_rep, reuse=False, keep_ph=self.keep_ph) 
-        self.caption_hardmax = tf.argmax(self.caption_logits, axis=-1)
+            self.caption_logits, own_caption_embs = _build_captioning_net(self.image_rep, reuse=False, keep_ph=self.keep_ph) 
+            self.caption_hardmax = tf.argmax(self.caption_logits, axis=-1)
 
-        self.caption_loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.caption_logits, labels=tf.one_hot(self.caption_ph, depth=vocab_size))
-        self.caption_train = self.optimizer.minimize(self.caption_loss)
+            masked_logits = tf.boolean_mask(self.caption_logits, self.mask_ph)
+
+            masked_labels = tf.boolean_mask(tf.one_hot(self.caption_ph, depth=vocab_size), self.mask_ph)
+
+            self.caption_loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=masked_logits, labels=masked_labels)
+            self.caption_loss = tf.reduce_sum(self.caption_loss)
+            self.caption_train = self.optimizer.minimize(self.caption_loss)
+
+        def _initialize_stuff():
+            # init, etc.
+            # set to initialize vision network from checkpoint
+            tf.contrib.framework.init_from_checkpoint(
+                config['vision_checkpoint_location'],
+                {'InceptionV3/': 'perception/InceptionV3/'})
+
+            sess_config = tf.ConfigProto()
+            sess_config.gpu_options.allow_growth = True
+            self.sess = tf.Session(config=sess_config) 
+
+            self.sess.run(tf.global_variables_initializer())
+
+        if config["no_consistency"]:
+            _initialize_stuff()
+
+        # caption reading and caption consistent net
+        with tf.variable_scope("consistency_evaluation"):
+
+            def _build_caption_processing_net(embedded_input, reuse=True, keep_ph=None):
+                """Processes caption from char embeddings"""
+                with tf.variable_scope('caption/reading', reuse=reuse):
+                    if keep_ph is not None:
+                        cell = lambda: tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.BasicLSTMCell(config['hidden_size']), output_keep_prob=keep_ph)
+                    else:
+                        cell = lambda: tf.contrib.rnn.BasicLSTMCell(config['hidden_size'])
+                    stacked_cell = tf.contrib.rnn.MultiRNNCell([cell() for _ in range(config['rnn_num_layers'])])
+
+                    state = stacked_cell.zero_state(config['batch_size'], tf.float32)
+                    with tf.variable_scope("recurrence", reuse=reuse):
+                        for step in range(config['sequence_length']):
+                            (output, state) = stacked_cell(embedded_input[:, step, :], state)
+                            tf.get_variable_scope().reuse_variables()
+
+                return output
+
+            def _build_caption_reading_net(caption_input, reuse=True, keep_ph=None):
+                """Reads caption and processes"""
+                with tf.variable_scope('caption/reading', reuse=reuse):
+                    embedded_input = tf.nn.embedding_lookup(input_embeddings, caption_input)
+                output = _build_caption_processing_net(embedded_input, reuse=reuse, keep_ph=keep_ph)
+                return output
+
+            def _build_caption_consistency_net(caption_1_rep, caption_2_rep, reuse=True, keep_ph=None):
+                """Evaluates whether captions represent the same image"""
+                net = slim.layers.fully_connected(tf.concat([caption_1_rep, caption_2_rep], -1), config["hidden_size"], activation_fn=tf.nn.relu)
+                net = slim.layers.fully_connected(net, 1, activation_fn=tf.nn.sigmoid)
+                return net
 
 
-        # init, etc.
-        # set to initialize vision network from checkpoint
-        tf.contrib.framework.init_from_checkpoint(
-            config['vision_checkpoint_location'],
-            {'InceptionV3/': 'perception/InceptionV3/'})
+            self.caption_rep = _build_caption_reading_net(tf.reverse(self.caption_ph, [-1]), reuse=False, keep_ph=self.keep_ph)
+            self.caption2_rep = _build_caption_reading_net(tf.reverse(self.caption2_ph, [-1]), reuse=True, keep_ph=self.keep_ph)
+            self.neg_caption_rep = _build_caption_reading_net(tf.reverse(self.neg_caption_ph, [-1]), reuse=True, keep_ph=self.keep_ph)
+            self.own_caption_rep = _build_caption_processing_net(tf.reverse(own_caption_embs, [-1]), reuse=True, keep_ph=self.keep_ph)
 
-        sess_config = tf.ConfigProto()
-        sess_config.gpu_options.allow_growth = True
-        self.sess = tf.Session(config=sess_config) 
+            self.caption_consistency_loss = tf.square(1-_build_caption_consistency_net(self.caption_rep, self.caption2_rep, False, self.keep_ph))
+            self.caption_consistency_loss += tf.square(_build_caption_consistency_net(self.caption_rep, self.neg_caption_rep, False, self.keep_ph))
 
-        self.sess.run(tf.global_variables_initializer())
+            self.own_caption_consistency_loss = tf.square(1-_build_caption_consistency_net(self.caption2_rep, self.own_caption_rep, False, self.keep_ph))
+
+        # images from captions
+
+
+        self.main_train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "basic_net/") 
+        self.other_train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "consistency_evaluation/") 
+#        print(self.main_train_vars)
+#        print(self.other_train_vars)
+        _initialize_stuff()
  
 
-    def _example_to_feeddict(self, example):
+    def _example_to_feeddict(self, example, negative_example=None):
         img = io.imread(example["image_name"]) 
         img = resize_image(rescale_image(img))
         captions = example["captions"] 
         np.random.shuffle(captions)
         feed_dict = {
              self.image_ph: np.expand_dims(img, axis=0), 
-             self.caption_ph: np.expand_dims(captions[0], 0),
-             self.caption2_ph: np.expand_dims(captions[1], 0)
+             self.caption_ph: np.expand_dims(captions[0]["caption"], 0),
+             self.mask_ph: captions[0]["mask"],
+             self.caption2_ph: np.expand_dims(captions[1]["caption"], 0),
+             self.mask2_ph: captions[1]["mask"]
         }
+        if negative_example is not None: # use a random caption from other as neg
+            feed_dict[self.neg_caption_ph]= np.expand_dims(
+                negative_example["captions"][np.random.randint(5)]["caption"], 0),
+            
         return feed_dict
 
-    def run_train_example(self, example):
+    def run_train_example(self, example, basic=False, negative_example=None):
         feed_dict = self._example_to_feeddict(example)
         feed_dict[self.keep_ph] = config["keep_prob"] 
         feed_dict[self.lr_ph] = self.curr_lr 
